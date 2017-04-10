336a1864c639eac7668de336a8fdeafc82d26616
isabelasilveira <isabela.wdev@gmail.com>
2017-04-09 22:37:39 -0300

    Ajustes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-jQuery(document).ready(function($){     
+function goToByScroll(id){ 
+    jQuery(document).ready(function($){
+        $('html,body').animate({scrollTop: $("#"+id).offset().top},'slow'); 
+       return false; 
+    });
+};
+jQuery(document).ready(function($){
     
     var larg = $('.painel').width();
     
@@ -16,11 +22,8 @@ jQuery(document).ready(function($){
         
         var mPosX = event.pageX;
         var mPosY = event.pageY;
-        console.log("Posicao X: ",mPosX);
-        console.log("Posicao Y: ",mPosY);
         
         if(mPosX <= larg/2){
-            console.log("menor que a metade");  
             $('#element1').css({'top':obj1.top - 30});
             $('#element2').css({'left':obj2.left + 30});
             $('#element3').css({'left':obj3.left - 35, 'top':obj3.top + 25});
@@ -31,7 +34,6 @@ jQuery(document).ready(function($){
             $('#element7').css({'left':obj7.left + 20, 'top':obj7.top - 25});
             $('#element8').css({'left':obj8.left + 35});   
         }else{
-            console.log("maior que a metade"); 
             $('#element1').css({'top':obj1.top + 15});
             $('#element2').css({'left':obj2.left - 30});
             $('#element3').css({'left':obj3.left + 35, 'top':obj3.top - 25});
@@ -70,15 +72,69 @@ jQuery(document).ready(function($){
     });
     
     $('#col-infos').addClass('animated slideInUp');
+    
+    //movimento dos itens
+    var wLarg = $('.lanches').width();
+    
+    var itens1 = $('#itens1').position();
+    var itens2 = $('#itens2').position();
+    var itens3 = $('#itens3').position();
+    var itens4 = $('#itens4').position();
+ 
+    $('.lanches').mousemove(function(){
+        
+        var mPosX = event.pageX;
+        var mPosY = event.pageY;
+        
+        if(mPosX <= wLarg/2){
+            $('#itens1').css({'top':itens1.top - 15});
+            $('#itens2').css({'left':itens2.left + 20});
+            $('#itens3').css({'top':itens3.top + 0});
+            $('#itens4').css({'top':itens4.top + 5});   
+        }else{
+            $('#itens1').css({'top':itens1.top + 15});
+            $('#itens2').css({'left':itens2.left - 20});
+            $('#itens3').css({'top':itens3.top + 25});
+            $('#itens4').css({'top':itens4.top - 5});
+        }
+        
+    });
+    
+    //movimento dos itens
+    var pLarg = $('.petiscos').width();
+    
+    var itens1 = $('#figure1').position();
+    var itens2 = $('#figure2').position();
+ 
+    $('.petiscos').mousemove(function(){
+        
+        var mPosX = event.pageX;
+        var mPosY = event.pageY;
+        
+        if(mPosX <= pLarg/2){
+            $('#figure1').css({'top':itens1.top - 15});
+            $('#figure2').css({'left':itens2.left + 20}); 
+        }else{
+            $('#figure1').css({'top':itens1.top + 15});
+            $('#figure2').css({'left':itens2.left - 20});
+        }
+        
+    });
 });
 
-var posElement = $('#col-infos').offset().top - 150;
-var altElement = $('#col-infos').height();
+var posElement = $('#col-infos').offset().top - 250;
+var altElement = $('#col-infos').height() + 250;
 var totalElement = posElement+altElement;
 
 var posSobre = $('.sobre').offset().top;
 var altSobre = $('.sobre').height();
 var totalSobre = posSobre+altSobre;
+
+var posAdd = $('.adicional').offset().top + 160;
+var altAdd = $('.adicional').height();
+var totalAdd = posAdd+altAdd;
+
+console.log(posAdd);
 
 var winScroll;
 jQuery(window).scroll(function(){
@@ -93,6 +149,12 @@ jQuery(window).scroll(function(){
     
     if( winScroll >= posSobre && winScroll <= totalSobre){
         $('.img-sobre').addClass('animated fadeIn');
+    }
+    
+    if( winScroll >= posAdd && winScroll <= totalAdd){
+        $('#bandeja').addClass('animated slideInUp').show();
+    }else if(winScroll < posAdd){
+        $('#bandeja').hide();        
     }
 
 })
