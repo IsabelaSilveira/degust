151d1caf8c7bd05d4abf60305ba5533e87178359
isabelasilveira <isabela.wdev@gmail.com>
2017-04-10 00:07:01 -0300

    Scroll

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ var posSobre = $('.sobre').offset().top;
 var altSobre = $('.sobre').height();
 var totalSobre = posSobre+altSobre;
 
-var posAdd = $('.adicional').offset().top + 160;
+var posAdd = $('.adicional').offset().top - 150;
 var altAdd = $('.adicional').height();
 var totalAdd = posAdd+altAdd;
 
@@ -157,4 +157,16 @@ jQuery(window).scroll(function(){
         $('#bandeja').hide();        
     }
 
-})
+});
+
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
